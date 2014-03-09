4b580425ee0daf927268f5b0f7b158f1b832ae25
Laurens <laurensloots@hotmail.com>
2014-03-09 11:25:16 +0100

    adding a new project

---
+++ Session5E4/src/PrimeNumberChecker.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+
+public class PrimeNumberChecker 
+{
+	public static void main(String[] args)
+	{
+		Scanner invoer = new Scanner(System.in);
+		System.out.println("Give a positive integer");
+		int posInt = invoer.nextInt();
+		int x = posInt - 1;
+		
+
+		if(posInt==2)
+		{
+			System.out.println("This is a prime number!");
+		}
+		
+		while(x>1)
+		{
+			
+			if(posInt % x==0)
+			{
+				System.out.println("This is not a prime number!");
+				x=1;
+			}
+			
+			else
+			{
+				if(x==2)
+				{
+					System.out.println("This is a prime number!");
+				}
+				x = x -1;
+			}
+		
+		}
+		
+		String primeNumbers = "These are the prime numbers: ";
+		int i;
+		
+		for(i = posInt; i>1; i=i-1)
+		{
+			x = i - 1;
+			while(x>1)
+			{
+			
+				if(i % x==0)
+				{
+					x=1;
+				}
+			
+				else
+				{
+					if(x==2)
+					{
+						primeNumbers = primeNumbers + i + ", ";
+					}
+					x = x -1;
+					
+				}
+		
+			}
+		}
+		if(posInt>2)
+		{
+			primeNumbers = primeNumbers + 2;
+		}
+		System.out.println(primeNumbers);
+		
+	}
+}
